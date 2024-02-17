6feb57f92d3f70c9615b582bc0b696690a7922b5
dhruvrastogi1999 <139809107+dhruvrastogi1999@users.noreply.github.com>
2024-02-17 22:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let isLeap = false;
+	if(year % 4 === 0){
+		//this may be a leap year
+		if(year % 100 ===0){
+			//centuary year
+			return year % 400 ===0 ? 366 : 365;
+		}
+		return 366;
+	}
+	return 365;
+}
